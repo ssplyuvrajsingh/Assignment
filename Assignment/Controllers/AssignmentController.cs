1f88b64fb11e8101d067f8b050ac6b1c267ce407
Shyam Agarwal <shyam@sumedhasoftech.com>
2021-08-11 17:12:42 +0530

    responds with the server time in ISO8601 format

--- Assignment/Controllers/AssignmentController.cs
+++ Assignment/Controllers/AssignmentController.cs
@@ -2,5 +2,14 @@
 using System.Web.Http;
 namespace Assignment.Controllers
 {
-   
+    public class AssignmentController : ApiController
+    {
+        // GET: Assignment
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetISO8601Format()
+        {
+            var newTime = DateTime.Now.Date.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+            return Json(newTime);
+        }
+    }
 }
